726bdb618f8d5b19401c194d3d682dff59995e02
Nilesh Patidar <nileshpatidar875@gmail.com>
2020-12-09 00:34:28 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ app.get("/api/posts",async (req,res)=>{
 			noOfPost=global.max;
 		}
 	}
-	if(noOfPost>15) noOfPost=15;
+	if(noOfPost>20) noOfPost=20;
 	if(global.count>5)
 	{
 		res.writeHead(429,{
